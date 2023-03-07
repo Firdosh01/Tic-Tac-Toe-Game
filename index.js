d1f59a69f2321075e552975cb02fadc6ab901619
Firdosh01 <firdosh7392@gmail.com>
2023-03-07 09:11:04 -0500

    logic Update

--- index.js
+++ index.js
@@ -2,45 +2,97 @@ const boxes = document.querySelectorAll(".box");
 const gameInfo = document.querySelector(".game-info")
 const newGamebtn = document.querySelector(".btn");
 
-let currentPlayer; 
+let currentPlayer;
 let gameGrid;
 
 const winningPositions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 
 function initGame() {
     currentPlayer = "X";
-    gameGrid = ["","","","","","","","",""];
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+
+        box.classList = `box box${index+1}`;
+    })
     newGamebtn.classList.remove("active");
-    gameInfo.innerHTML = `Current player -  ${currentPlayer}`
+    gameInfo.innerText = `Current player -  ${currentPlayer}`;
 }
 initGame()
 
-function swapTurn () {
-    if(currentPlayer ==="X") {
-        currentPlayer = "0"
-    }else{
+function swapTurn() {
+    if (currentPlayer === "X") {
+        currentPlayer = "O"
+    } else {
         currentPlayer = "X"
     }
-        //UI Update
-        gameInfo.innerText = `Current Player - ${currentPlayer}`;
+    //UI Update
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+function checkGameOver() {
+    // newGamebtn.classList.add("active");
+    let answer = "";
+
+    winningPositions.forEach((position) => {
+        if ((gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "")
+            && (gameGrid[position[0]] === gameGrid[position[1]]) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+
+            if (gameGrid[position[0]] === "X") {
+                answer = "X";
+            } else {
+                answer = "O"
+            }
+
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            })
+
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+
+        }
+    });
+
+    if (answer !== "") {
+        gameInfo.innerText = `Winner Player - ${answer}`;
+        newGamebtn.classList.add("active")
+        return;
+    }
+
+    //We know, NO Winner Found, let's check whether there is tie
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if (box !== "")
+            fillCount++;
+    });
+
+    //board is Filled, game is TIE
+    if (fillCount === 9) {
+        gameInfo.innerText = "Game Tied !";
+        newGamebtn.classList.add("active");
+    }
 }
 
 function handleClick(index) {
-    if(gameGrid[index] === "") {
+    if (gameGrid[index] === "") {
         boxes[index].innerText = currentPlayer;
         gameGrid[index] = currentPlayer;
         boxes[index].style.pointerEvents = "none";
-       
+
         // swap karo turn ko 
         swapTurn();
 
@@ -54,5 +106,6 @@ boxes.forEach((box, index) => {
     box.addEventListener("click", () => {
         handleClick(index)
     })
-})
-newGameBtn.addEventListener("click", initGame);
+});
+
+newGamebtn.addEventListener("click", initGame);
